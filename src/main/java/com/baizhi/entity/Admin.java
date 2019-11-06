3a15b05ae40cfd3f7d996123eea52e608934bedd
Glb <2745517205@qq.com>
2019-11-06 11:50:03 +0800

    这是测试

--- src/main/java/com/baizhi/entity/Admin.java
+++ src/main/java/com/baizhi/entity/Admin.java
@@ -19,5 +19,22 @@ public class Admin {
     private String nickname;
     private Integer age;
     private String sex;
-    ptivate String test;
+    private String test;
+
+    public static void main(String[] args) {
+        int[] a = {1, 4, 7, 5, 2, 3};
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - 1 - i; j++) {
+                if (a[j] < a[j + 1]) {
+                    count = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = count;
+                }
+            }
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
 }
